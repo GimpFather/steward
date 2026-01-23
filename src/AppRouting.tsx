5bdd5d183b7f6bafa0a0f732e515c20b8e990e3b
Filip Galczyk <filip.galczyk13@gmail.com>
2026-01-23 21:08:21 +0100

    Usunięcie framer-motion

--- src/AppRouting.tsx
+++ src/AppRouting.tsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router"
 import AppLayout from "./components/UI/AppLayout"
-import { AnimatePresence } from "motion/react"
 import PaymentsPage from "./views/PaymentsPage.view"
 import CategoriesPage from "./views/CategoriesPage.view"
 import DeepDivePage from "./views/DeepDive.view"
@@ -8,16 +7,14 @@ import ExpensesPage from "./views/ExpensesPage.view"
 
 const AppRouting = () => {
     return (
-        <AnimatePresence mode="wait" initial={true}>
-            <Routes location={location} key={location.pathname}>
-                <Route element={<AppLayout />}>
-                    <Route path="/" element={<ExpensesPage />} />
-                    <Route path="/payments" element={<PaymentsPage />} />
-                    <Route path="/categories" element={<CategoriesPage />} />
-                    <Route path="/deep-dive" element={<DeepDivePage />} />
-                </Route>
-            </Routes>
-        </AnimatePresence>
+        <Routes location={location} key={location.pathname}>
+            <Route element={<AppLayout />}>
+                <Route path="/" element={<ExpensesPage />} />
+                <Route path="/payments" element={<PaymentsPage />} />
+                <Route path="/categories" element={<CategoriesPage />} />
+                <Route path="/deep-dive" element={<DeepDivePage />} />
+            </Route>
+        </Routes>
     )
 }
 
